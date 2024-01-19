3cc3a85d3cc234ef7b2746ee6ef26ea88a5c7ca3
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-01-19 03:11:06 +0200

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,9 +1,9 @@
--- lists all bands with Glam rock as their main style, ranked by their longevity
+-- last modification date: 2019-01-05
 
 
-SELECT band_name, 
-CASE 
-    WHEN split IS NULL 
+SELECT band_name,
+CASE
+    WHEN split IS NULL
     THEN 2022 - formed
     ELSE split - formed
 END AS lifespan
