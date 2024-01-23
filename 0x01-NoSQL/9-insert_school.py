5d05ea744ba0e809d6fec335f3850c34c23af46f
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-01-24 00:35:12 +0200

    Create 9-insert_school.py

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""module docs"""
+
+
+def insert_school(mongo_collection, **kwargs) -> str:
+    """insert a document in a collection based on kwargs"""
+    return mongo_collection.insert_one(kwargs).inserted_id
